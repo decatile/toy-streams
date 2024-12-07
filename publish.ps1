8427b36bfa030277c9556784dbb4635a3f0fecf2
catile <ohmycatile@gmail.com>
2024-12-07 19:56:42 +0300

    fixed ps script

--- publish.ps1
+++ publish.ps1
@@ -2,10 +2,29 @@ param(
   [String] $Message,
   [String] $Version = "patch"
 )
-git stash
-npm version $Version
-git stash pop
-git add .
-git commit -m $Message
-git push
-npm publish
+
+function C {
+  param (
+    [String] $Call
+  )
+  Invoke-Expression $Call
+  if ($LASTEXITCODE -ne 0) {
+    throw "$Call failed with $LASTEXITCODE"
+  }
+}
+
+if ($Message -eq "") {
+  throw "-Message is required"
+}
+
+if ($Version -notin @("patch", "minor", "major")) {
+  throw "-Version should be 'patch', 'minor' or 'major'"
+}
+
+C "git stash"
+C "npm version $Version"
+C "git stash pop"
+C "git add ."
+C "git commit -m '$Message'"
+C "git push"
+C "npm publish"
